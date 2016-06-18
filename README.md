b27bc256e7b0e2857519c5b9de3cc02217995bad
Jay Lin <weijingjaylin@gmail.com>
2016-06-18 14:27:07 -0700

    installed

---
+++ README.md
@@ -0,0 +1,6 @@
+# tencentyun-image-admin
+
+## Usage
+1. 使用 composer 来安装
+
+`composer install`
--- index.php
+++ index.php
@@ -1,9 +1,12 @@
+<?php 
+  include_once('./vendor/autoload.php');
+?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>腾讯云管理系统</title>
+    <title>腾讯云万象优云管理系统</title>
     <link href="/lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"/>
     <link href="/lib/dpatch/dist/css/general-cn.css" rel="stylesheet"/>
     <script src="/lib/angular/angular.js"></script>
@@ -12,7 +15,7 @@
   </head>
   <body>
     <div class="container">
-      <h1>TITLE</h1>
+      <h1>腾讯云万象优图管理系统</h1>
     </div>
   </body>
 </html>
